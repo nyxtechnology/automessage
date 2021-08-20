9cf0440af860085b6635fae559c7f1678fd40cbb
DavidLordCaos <davidlucas807@yahoo.com.br>
2021-08-20 11:43:06 -0300

    correcter error with mailgun

--- app/EmailSchedulings.php
+++ app/EmailSchedulings.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailSchedulings extends Model
 {
-    protected $fillable = ['id', 'external_id', 'from', 'from_name', 'to', 'to_name', 'subject', 'body', 'delivery_date', 'template_variables', 'sent', 'event_stop'];
-    protected $casts = ['id' => 'string', ];
+    public $fillable = ['id', 'external_id', 'from', 'from_name', 'to', 'to_name', 'subject', 'body', 'delivery_date', 'template_variables', 'sent', 'event_stop'];
+    public $casts = ['id' => 'string', ];
 }
