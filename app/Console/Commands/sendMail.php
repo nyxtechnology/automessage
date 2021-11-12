309bfcb18ac3398977ca4ba04d420da45fd26529
DavidLordCaos <davidlucas807@yahoo.com.br>
2021-11-12 09:30:41 -0300

    command to send email and controller to redirect to the mailhog page

---
+++ app/Console/Commands/sendMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Mail\mailTest;
+use Illuminate\Support\Facades\Mail;
+
+class sendMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Mail::to('saul@hudson.com')->send(new maiLTest());
+    }
+}
