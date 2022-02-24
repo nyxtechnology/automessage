67b43de11a25ce97259dcc43cb93f5586084dcf3
gilbertoss <gilberto.s.silveira@gmail.com>
2022-02-24 09:48:39 +0000

    Update event configuration file to use json.

--- app/Http/Controllers/ReceiverController.php
+++ app/Http/Controllers/ReceiverController.php
@@ -2,37 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\WebhookReceived;
 use App\Jobs\ReceiveEvent;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
-use stdClass;
 
 class ReceiverController extends Controller
 {
-  public function handleWebhook(Request $request){
-      //TODO: migrate event file to db
-
-      // check if event is mapped
-      if(!Config::get('eventsMap.' . $request->json('event'))){
-          Log::error('Event not mapped: ' . $request->json('event'));
-          return response('Event not mapped.', 404);
-      }
-
-      $message = new stdClass();
-      $message->event = $request->json('event');
-      $message->metadata = $request->json('metadata');
-
-      // send event to queue
-      try{
-          ReceiveEvent::dispatch(json_encode($message))->onConnection(Config::get('queue.default'));
-      }catch(\Exception $e){
-          Log::error('Error send event to queue: ' . $e->getMessage());
-          return response('We have a problem! Try again later.', 500);
-      }
-
-      return response('We have received your event and will process it shortly if everything is ok.', 200);
-  }
+    public function handleWebhook(Request $request){
+        // checks if the event configuration file exists
+        if(file_exists(Config::get('settings.event_path'))){
+            $events = json_decode(file_get_contents(Config::get('settings.event_path')),true);
+            // check if event is mapped
+            if(!array_key_exists($request->json('event'), $events)){
+                Log::error('Event not mapped: ' . $request->json('event'));
+                return response('Sorry! This event is not mapped.', 404);
+            }
+            // send event to queue
+            try{
+                ReceiveEvent::dispatch($request->getContent(), true)->onConnection(Config::get('queue.default'));
+            }catch(\Exception $e){
+                Log::error('Error send event to queue: ' . $e->getMessage());
+                return response('Houston, we have a problem!', 500);
+            }
+            return response('We have received your event and will process it shortly if everything is ok.', 200);
+        }
+    }
 }
 
