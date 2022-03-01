cd08b5333d5a8b27423b39fd36d16b344023d55b
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-03-01 13:27:04 -0300

    changed email example template to using data from POST instead fixed parameters

--- resources/views/mails/example.blade.php
+++ resources/views/mails/example.blade.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Email de teste</title>
-</head>
 <body>
-    <h1>Hello Saul!</h1>
-    <h3>Greetings from Steve.</h3>
+    <h1>Hello {{ $name }}</h1>
+    {{ $body }}
 </body>
 </html>
