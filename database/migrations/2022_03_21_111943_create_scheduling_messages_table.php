33c210dab6ed14574a4a42c4d0de9d0ae2523fab
gilbertoss <gilberto.s.silveira@gmail.com>
2022-03-21 15:42:48 +0000

    Delete old files and add new files to scheduling message.

---
+++ database/migrations/2022_03_21_111943_create_scheduling_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulingMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scheduling_messages', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->json('data');
+            $table->json('conditions_stop')->nullable();
+            $table->json('conditions_update')->nullable();
+            $table->boolean('sent')->default(false);
+            $table->date('delivery_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scheduling_messages');
+    }
+}
