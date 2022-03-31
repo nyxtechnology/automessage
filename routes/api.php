14db46ae32929686ec717205b68fa379d7ba018c
gilbertoss <gilberto.s.silveira@gmail.com>
2022-03-31 15:10:13 +0100

    Add webhook url key.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('webhook', 'ReceiverController@handleWebhook');
+Route::post('webhook/' . Config::get('settings.webhook_key'), 'ReceiverController@handleWebhook');
 
 Route::post('telegram', 'TelegramController@receiveMessage');
